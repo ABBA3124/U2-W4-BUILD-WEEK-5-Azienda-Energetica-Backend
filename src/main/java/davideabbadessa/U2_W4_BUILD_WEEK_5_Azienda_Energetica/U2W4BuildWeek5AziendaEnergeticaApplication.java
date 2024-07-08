a1510671a763b5bf0f56e0834f8bd7f82e530708
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-08 10:31:18 +0200

    Initial commit

---
+++ src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/U2W4BuildWeek5AziendaEnergeticaApplication.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W4BuildWeek5AziendaEnergeticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W4BuildWeek5AziendaEnergeticaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/U2W4BuildWeek5AziendaEnergeticaApplicationTests.java
@@ -0,0 +1,13 @@
+package davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W4BuildWeek5AziendaEnergeticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
