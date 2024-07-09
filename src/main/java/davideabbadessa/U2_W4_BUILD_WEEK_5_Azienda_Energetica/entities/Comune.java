d7442bafbd9b86077d16bd0453d802340c3ef547
Francesco Cristiano <frankrys97@gmail.com>
2024-07-09 16:10:35 +0200

    adds JsonIgnoreProperties for Comuni and Province

--- src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/entities/Comune.java
+++ src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/entities/Comune.java
@@ -1,5 +1,6 @@
 package davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Data
 @Entity
 @NoArgsConstructor
+@JsonIgnoreProperties({"indirizzi"})
 public class Comune {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
