dd8c2c615fff8a45c6162f015f49cfea1e1136f1
Francesco Cristiano <frankrys97@gmail.com>
2024-07-09 10:20:48 +0200

    Update Ruolo.java

--- src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/entities/Ruolo.java
+++ src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/entities/Ruolo.java
@@ -12,10 +12,14 @@ public class Ruolo {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    
+
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @ManyToMany(mappedBy = "ruoli")
     private Set<Utente> utenti;
+
+    public Ruolo(Role role) {
+        this.role = role;
+    }
 }
