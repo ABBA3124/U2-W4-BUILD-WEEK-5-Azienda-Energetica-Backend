e1f3234b7ada9ec8f0779bb214e77ad50110df47
Francesco Cristiano <frankrys97@gmail.com>
2024-07-09 15:47:45 +0200

    adds fixes about register and login

--- src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/services/UtenteService.java
+++ src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/services/UtenteService.java
@@ -1,18 +1,27 @@
 package davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.services;
 
+import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.entities.Role;
 import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.entities.Utente;
 import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.exceptions.NotFoundException;
 import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.payloads.NewUtenteDTO;
 import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.repositories.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class UtenteService {
     @Autowired
     private UtenteRepository utenteRepository;
+
+    @Autowired
+    private RuoloService ruoloService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public Utente findById(UUID utenteId) {
         return this.utenteRepository.findById(utenteId).orElseThrow(() -> new NotFoundException(utenteId));
@@ -23,7 +32,8 @@ public class UtenteService {
     }
 
     public Utente save(NewUtenteDTO nuovoUtenteDTO) {
-        Utente nuovoUtente = new Utente(nuovoUtenteDTO.username(), nuovoUtenteDTO.email(), nuovoUtenteDTO.password(), nuovoUtenteDTO.nome(), nuovoUtenteDTO.cognome());
+        Utente nuovoUtente = new Utente(nuovoUtenteDTO.username(), nuovoUtenteDTO.email(), passwordEncoder.encode(nuovoUtenteDTO.password()), nuovoUtenteDTO.nome(), nuovoUtenteDTO.cognome());
+        nuovoUtente.setRuoli(Set.of(ruoloService.findByRole(Role.USER)));
         return utenteRepository.save(nuovoUtente);
     }
 }
