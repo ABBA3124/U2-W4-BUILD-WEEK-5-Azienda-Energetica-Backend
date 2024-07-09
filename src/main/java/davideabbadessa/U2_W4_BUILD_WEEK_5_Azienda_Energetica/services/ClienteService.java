2f33f0b1e8ef9de16aea33828f68300c270b0452
ABBA3124 <davideabbadessa73@gmail.com>
2024-07-09 14:31:05 +0200

    Aggiornato ClienteService
    aggiunto metodo getClienti in ClienteService

--- src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/services/ClienteService.java
+++ src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/services/ClienteService.java
@@ -1,8 +1,13 @@
 package davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.services;
 
 
+import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.entities.Cliente;
 import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +15,12 @@ public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    public Page<Cliente> getClienti(int pageNumber, int pageSize, String sortby) {
+        if (pageSize > 100) pageSize = 100;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortby));
+        return clienteRepository.findAll(pageable);
+    }
 
 
 }
