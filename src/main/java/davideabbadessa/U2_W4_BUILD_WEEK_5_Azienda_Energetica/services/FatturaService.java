82fc61beb3e30d8a8f4ee61247283e69a1ec09c3
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-07-11 13:12:48 +0200

    Fattura service per i filtri fatta

--- src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/services/FatturaService.java
+++ src/main/java/davideabbadessa/U2_W4_BUILD_WEEK_5_Azienda_Energetica/services/FatturaService.java
@@ -7,6 +7,10 @@ import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.enums.StatusFattura
 import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.payloads.NewFatturaDTO;
 import davideabbadessa.U2_W4_BUILD_WEEK_5_Azienda_Energetica.repositories.FatturaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -20,10 +24,33 @@ public class FatturaService {
     @Autowired
     private StatoFatturaService statoFatturaService;
 
+    public Page<Fattura> getFattura(int pageNumber, int pageSize, String sortby) {
+        if (pageSize > 100) pageSize = 100;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortby));
+        return fatturaRepository.findAll(pageable);
+    }
+
     public Fattura save(NewFatturaDTO body) {
         Cliente clienteFattura = clienteService.findByRagioneSociale(body.cliente());
         StatoFattura statoFattura = statoFatturaService.findByStatus(StatusFattura.getStatoFattura(body.statoFattura()));
         Fattura nuovaFattura = new Fattura(LocalDate.parse(body.data()), body.importo(), body.numero(), clienteFattura, statoFattura);
         return fatturaRepository.save(nuovaFattura);
+    }
+
+    public Page<Fattura> trovaTutteLeFattureConFiltri(String nome,
+                                                      StatoFattura statoFattura,
+                                                      LocalDate dataMin,
+                                                      LocalDate dataMax,
+                                                      Integer annoMin,
+                                                      Integer annoMax,
+                                                      Double importoMin,
+                                                      Double importoMax,
+                                                      int pageNumber,
+                                                      int pageSize,
+                                                      String sortby) {
+        if (pageSize > 100) pageSize = 100;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortby));
+        return fatturaRepository.findWithFilters(nome, statoFattura,
+                dataMin, dataMax, annoMin, annoMax, importoMin, importoMax, pageable);
     }
 }
